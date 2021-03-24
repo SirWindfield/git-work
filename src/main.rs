6d13b664743efa10e00a2c3f134a803f2ef15b05
SirWindfield <SirWindfield@users.noreply.github.com>
2021-03-24 13:18:55 +0100

    chore: implement branch creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use clap::{App, AppSettings, Clap, IntoApp};
 use clap_generate::generators::{Bash, Elvish, Fish, PowerShell, Zsh};
 use clap_generate::{generate, Generator};
@@ -16,6 +17,10 @@ struct Cli {
 #[derive(Clap, Debug)]
 #[clap(author, setting(AppSettings::ColoredHelp), version)]
 struct CommitTypeArgs {
+    /// Force the creation of a branch even if it exists.
+    #[clap(long)]
+    force: bool,
+
     title: Vec<String>,
 }
 
@@ -54,23 +59,40 @@ macro_rules! gen_match {
                 SubCommand::$variant(args) => {
                     let commit_type = stringify!($variant).to_lowercase();
                     let branch_name = format!("{}/{}", commit_type, args.title.join(" ").to_kebab_case());
-                    create_branch(&branch_name).expect(&format!("Failed to create branch: {}", branch_name));
+                    create_branch(&branch_name, args.force).expect(&format!("Failed to create branch: {}", branch_name));
                 }
             )*
         }
     }
 }
 
-fn create_branch(name: &str) -> Result<(), ()> {
-    println!("Creating branch: {}", name);
+fn create_branch(name: &str, force: bool) -> Result<()> {
+    let current_dir = std::env::current_dir()?;
+
+    // Open repository and find latest commit hash.
+    let repo = git2::Repository::open(&current_dir)?;
+    let head = repo.head()?;
+    let oid = head
+        .target()
+        .expect("Failed to get commit id of current branch");
+    let commit = repo.find_commit(oid)?;
+
+    // Create the new branch and check it out.
+    let reference = format!("refs/heads/{}", name);
+    let _ = repo.branch(name, &commit, force)?;
+    repo.set_head(&reference)?;
+
+    println!("Switched branch to '{}'", name);
     Ok(())
 }
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     // Check if completions should be generated.
     if let Some(cmd) = cli.cmd {
         gen_match!(cmd, Build, Chore, Ci, Docs, Feat, Fix, Perf, Refactor, Revert, Style, Test)
     }
+
+    Ok(())
 }
